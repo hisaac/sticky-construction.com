6ef951147b7bc509eede0b4c83b1842bdfd5516a
Isaac Halvorson <hello@hisaac.net>
2023-03-31 10:05:49 -0500

    Various cleanup (#1)

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# sticky.construction
+# sticky-construction.com
 
 [sticky-construction.com](http://sticky-construction.com) is the homepage of Sticky Construction, Inc., a commercial caulking company from Thompson, ND.
 
--- gulpfile.js
+++ gulpfile.js
@@ -1,40 +1,43 @@
-var gulp = require('gulp');
-var browserSync = require('browser-sync').create();
-var pkg = require('./package.json');
+var gulp = require("gulp");
+var browserSync = require("browser-sync").create();
+var pkg = require("./package.json");
 
 // Copy vendor files from /node_modules into /vendor
 // NOTE: requires `npm install` before running!
-gulp.task('copy', function() {
-  gulp.src([
-      'node_modules/bootstrap/dist/**/*',
-      '!**/npm.js',
-      '!**/bootstrap-theme.*',
-      '!**/*.map'
-    ])
-    .pipe(gulp.dest('vendor/bootstrap'))
+gulp.task("copy", function () {
+	gulp.src([
+		"node_modules/bootstrap/dist/**/*",
+		"!**/npm.js",
+		"!**/bootstrap-theme.*",
+		"!**/*.map",
+	]).pipe(gulp.dest("src/vendor/bootstrap"));
 
-  gulp.src(['node_modules/jquery/dist/jquery.js', 'node_modules/jquery/dist/jquery.min.js'])
-    .pipe(gulp.dest('vendor/jquery'))
+	gulp.src([
+		"node_modules/jquery/dist/jquery.js",
+		"node_modules/jquery/dist/jquery.min.js",
+	]).pipe(gulp.dest("src/vendor/jquery"));
 
-  gulp.src(['node_modules/popper.js/dist/umd/popper.js', 'node_modules/popper.js/dist/umd/popper.min.js'])
-    .pipe(gulp.dest('vendor/popper'))
-})
+	gulp.src([
+		"node_modules/popper.js/dist/umd/popper.js",
+		"node_modules/popper.js/dist/umd/popper.min.js",
+	]).pipe(gulp.dest("src/vendor/popper"));
+});
 
 // Default task
-gulp.task('default', ['copy']);
+gulp.task("default", ["copy"]);
 
 // Configure the browserSync task
-gulp.task('browserSync', function() {
-  browserSync.init({
-    server: {
-      baseDir: ''
-    },
-  })
-})
+gulp.task("browserSync", function () {
+	browserSync.init({
+		server: {
+			baseDir: "",
+		},
+	});
+});
 
 // Dev task with browserSync
-gulp.task('dev', ['browserSync'], function() {
-  // Reloads the browser whenever HTML or CSS files change
-  gulp.watch('css/*.css', browserSync.reload);
-  gulp.watch('*.html', browserSync.reload);
+gulp.task("dev", ["browserSync"], function () {
+	// Reloads the browser whenever HTML or CSS files change
+	gulp.watch("src/css/*.css", browserSync.reload);
+	gulp.watch("src/*.html", browserSync.reload);
 });
